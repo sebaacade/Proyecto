bb063be965e43db534cc45dfbaafe9ffbab21c5b
Seba <sebastianvaras234192@alumnos.info.unlp.edu.ar>
2025-05-15 16:44:48 -0300

    agregue el caso de uso ListarEventoDeportivoConCupoDisponibleUseCase

---
+++ CentroEventos.Aplicacion/CasosDeUso/ListarEventoDeportivoConCupoDisponibleUseCase.cs
@@ -0,0 +1,18 @@
+namespace CentroEventos.Aplicacion;
+
+public class ListarEventoDeportivoConCupoDisponibleUseCase(IRepositorioEventoDeportivo repositorioEventoDeportivo, IRepositorioReserva repositorioReserva)
+{
+
+    public List<EventoDeportivo> Ejecutar()
+    {
+        List<EventoDeportivo> listado = new List<EventoDeportivo>();
+        List<EventoDeportivo> temp = repositorioEventoDeportivo.ListarEventoDeportivo();
+        foreach (EventoDeportivo e in temp){
+            if (repositorioReserva.CantidadDeReservas(e.Id) < e.CupoMaximo && e.FechaHoraInicio > DateTime.Now)
+            {
+                listado.Add(e);
+            }
+        }    
+            return listado;
+    }
+}
